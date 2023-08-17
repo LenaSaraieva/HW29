da077e3eeb4ce762fd86b218126188b06e80bf73
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-08-17 14:44:53 +0300

    the same resolution

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ const imageNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg
 const randomImageName = imageNames[Math.floor(Math.random() * imageNames.length)];
 
 randomImage.src = "./images/" + randomImageName;
-}
-);
+});
